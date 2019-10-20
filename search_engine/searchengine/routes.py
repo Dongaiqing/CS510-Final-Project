77fd7c9330437996b568d2e6f1d8c6e063a6b03f
dongaiiqng <dongaiiqng@gmail.com>
2019-10-20 01:20:47 -0500

    Added relevance selection and link click logging endpoints

--- search_engine/searchengine/routes.py
+++ search_engine/searchengine/routes.py
@@ -12,12 +12,29 @@ def search():
     results = model.query(req['query'], 10)
     res = list({'title': result[0], 'abstract': result[1]} for result in results)
     
-    return Response(response=json.dumps(res), status=200, mimetype="application/json")
+    return Response(response=json.dumps(res), 
+                    status=200, 
+                    mimetype="application/json")
+
+@app.route("/relevance-selection", methods=["POST"])
+def rel_selection_log():
+    req = request.get_json()
+    id = req['id']
+    query = req['query']
+    rel = req['rel']
+
+    print("RELEVANCE SELECTION EVENT: paper {} marked {} to query '{}'.".format(id, "relevant" if rel == 1 else "irrelevant", query))
+    return "Logged", 200
+
+@app.route("/link-click", methods=["POST"])
+def link_click_log():
+    req = request.get_json()
+    id = req['id']
+    query = req['query']
+
+    print("LINK CLICK EVENT: paper {} clicked under query '{}'.".format(id, query))
+    return "Logged", 200
 
 @app.route("/")
 def root():
     return render_template('index.html')
-
-@app.route("/result")
-def rank():
-    return render_template('result.html')
