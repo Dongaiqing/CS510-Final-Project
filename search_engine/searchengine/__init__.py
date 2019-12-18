e2e87146d9aebe7256568d561c86d40050109ba5
dongaiiqng <dongaiiqng@gmail.com>
2019-12-18 17:43:35 -0600

    Added comments

--- search_engine/searchengine/__init__.py
+++ search_engine/searchengine/__init__.py
@@ -4,19 +4,13 @@ app = Flask(__name__,
             static_url_path='', 
             static_folder='static')
 
-# DB config
+# DB config, 'MYSQL_DB' should not be changed
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '123123'
 app.config['MYSQL_DB'] = 'SearchEngine'
 
-# from utils import os_directory
-# from utils.data_loader import DataLoader
-# from searchengine.engine.model import UnigramLM
 from .engine.db_controller import DBController
-# loader = DataLoader()
-# num_files = loader.load_data()
-# model = UnigramLM(loader.destination_dir, num_files, 0.1)
 
 db_controller = DBController()
 
